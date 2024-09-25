1fa1291c4f4bc937aff6259fb2862a5841363681
Yan Brunshteyn <brunshte@gmail.com>
2024-09-25 05:50:46 -0700

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Namer Mac on 9/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
